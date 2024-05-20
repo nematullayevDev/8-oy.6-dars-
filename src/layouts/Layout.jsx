18c2b7ccf9ad31b0aea5fff4b183be59f01390ae
nematullayevDev <webdargon@gmail.com>
2024-05-20 14:50:33 +0500

    8-oy.6-dars

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -5,7 +5,6 @@ import styled from "@emotion/styled";
 function Layout({ children }) {
   const LayoutWrapper = styled.div`
     display: flex;
-
     width: 100%;
     justify-content: space-between;
   `;
